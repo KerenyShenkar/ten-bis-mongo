900b481ec565ea58bf8cd72b24cbe63de6957735
KerenyX <kereny82@gmail.com>
2020-11-27 06:41:14 +0200

    fix morgan

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const logger = require("morgan"); // NOTE: for debugging
+// const logger = require("morgan"); // NOTE: for debugging
 const app = express();
 const port = process.env.PORT || 3000;
 const { orderRouter, restaurantRouter, userRouter } = require("./router");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(logger("dev")); // app.use(logger("combined"));
+// app.use(logger("dev")); // app.use(logger("combined"));
 
 app.use('/api/restaurants', restaurantRouter);
 // app.use('/api/users', userRouter);
