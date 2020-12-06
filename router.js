840a941eb2fabacc3654c25c79c69a8d97a59cd8
KerenyX <kereny82@gmail.com>
2020-12-06 12:41:07 +0200

    ten-bis class example with mongo-db

--- router.js
+++ router.js
@@ -1,26 +1,19 @@
 const { Router } = require('express');
-const { orderController, userController, restaurantController } = require('./controller');
+const { orderController, userController } = require('./controller');
 
-const restaurantRouter = new Router();
-// const userRouter = new Router();
-// const orderRouter = new Router();
+const userRouter = new Router();
+const orderRouter = new Router();
 
-restaurantRouter.get('/', restaurantController.getRestaurants);         // localhost:3000/restaurants
-// restaurantRouter.get('/:id', restaurantController.getRestaurant);       // localhost:3000/restaurants/5
-restaurantRouter.post('/', restaurantController.addRestaurant);         // localhost:3000/restaurants (with restaurant object)
-// restaurantRouter.put('/:id', restaurantController.updateRestaurant);    // localhost:3000/restaurants/38 (with restaurant object)
-//restaurantRouter.delete('/:id', restaurantController.deleteRestaurant); // localhost:3000/restaurants/43
+userRouter.get('/', userController.getUsers);
+userRouter.get('/:id', userController.getUser);
+userRouter.post('/', userController.addUser);
+userRouter.put('/:id', userController.updateUser);
+userRouter.delete('/:id', userController.deleteUser);
 
-// userRouter.get('/', userController.get);
-// userRouter.get('/:id', userController.get);
-// userRouter.post('/', userController.post);
-// userRouter.put('/:id', userController.put);
-// userRouter.delete('/:id', userController.delete);
+orderRouter.get('/', orderController.getOrders);
+orderRouter.get('/:id', orderController.getOrder);
+orderRouter.post('/', orderController.addOrder);
+orderRouter.put('/:id', orderController.updateOrder);
+orderRouter.delete('/:id', orderController.deleteOrder);
 
-// orderRouter.get('/', orderController.get);
-// orderRouter.get('/:id', orderController.get);
-// orderRouter.post('/', orderController.post);
-// orderRouter.put('/:id', orderController.put);
-// orderRouter.delete('/:id', orderController.delete);
-
-module.exports = { restaurantRouter };
+module.exports = { userRouter, orderRouter };
