f1e644078452f5c8ead4294d086d7f6935c913d9
KerenyX <kereny82@gmail.com>
2020-12-06 12:43:19 +0200

    removed un-used restaurants data

--- data.js
+++ data.js
@@ -1,9 +1,7 @@
-const restaurants = require('./mock.data/restaurants');
 const users = require('./mock.data/users');
 const orders = require('./mock.data/orders');
 
 module.exports = {
-    restaurants,
     users,
     orders
 };
